dbab4bad6f8d89008c252c4f59884727aa0b9d7f
Siddhant Maurya <88020811+sidhthecoder@users.noreply.github.com>
2023-05-01 16:17:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 function convertToRoman(num) {
   	const obj = {
-    0: ['M', 1000],
-    1: ['D', 500],
-    2: ['C', 100],
-    3: ['L', 50],
-    4: ['X', 10],
-    5: ['V', 5],
-    6: ['I', 1],
-    7: ['CM', 900],
-    8: ['XC', 90],
-    9: ['IX', 9]
-  };
+      0:['M',1000], 
+      1:['D', 500], 
+      2:['C', 100], 
+      3:['L', 50], 
+      4:['X', 10], 
+      5:['V', 5], 
+      6:['I', 1]
+    };
     let romanNumeral = "";
 
     for (let key in obj) {
 	  if (num >= obj[key][1] && obj[key][0].length === 2) {
 	    romanNumeral += obj[key][0];
 	    num -= obj[key][1];
-	 }
+	}
 
 	while (num >= obj[key][1]) {
 	  romanNumeral += obj[key][0];
